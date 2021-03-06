3a53e3f4e7fd0eef7da33df713a42a113c4f2e27
Barrera <barreraalexander93@gmail.com>
2021-03-06 17:31:51 -0500

    adds

--- website/components/ItemDiv.py
+++ website/components/ItemDiv.py
@@ -1,43 +1,22 @@
 from flask import Markup, url_for, redirect
 from website.models.item import Item
 
-
 def component(model):
-    def get_sell_div(model):
-        img_src = url_for('static', filename=f'images/item_images/{model._id}/thumbnails/{model.imgfiles[0]}')
-        a_src = url_for('users.inv_item', model_id=model._id)
+        img_src = url_for('static', filename=f'images/item_images/{model._id}/{model.imgfiles[0]}')
+        a_src = url_for('main.inv_item', model_id=model._id)
         
-        return Markup(f"""
-        <h5> {model.name} </h5>
-        <div class='sell_divs_ctnr'>
-            <div class='sell_img_div'>
-                <img src="{img_src}" alt="merch_img">
-            </div>
-
-            <div>
-                <p class='sell_head'>
-                    Quantity
-                </p>
+        return Markup(f""" 
+        <div class='item_flexy_ctnr'>
+            <img src={img_src} alt={model.name}>
+            <a href={a_src}>
                 <p>
-                    Quantity
+                    {model.name}
                 </p>
-            </div>
-
-            <div>
-                <p class='sell_head'>
-                    Total
-                </p>
-                <p>
-                    {model.selling_price}
-                </p>
-            </div>
-
-            <div>
-                <button id="remove_btn_shown"  onclick=submitRemove("{model._id}") class="remove_button">
-                    REMOVE
-                </button>
-            </div>
-            
+            </a>
+            <p class='flex_item_price'> ${model.selling_price} </p>
+            <p class='flex_item_shipping'> Shipping: ${model.shipping_price} </p>
+            <button class='btn_filled flexy_cta' onclick=submitAdd("{model._id}")>
+                Add to Cart
+            </button>
         </div>
-        <hr>
-        """)
+                """)
